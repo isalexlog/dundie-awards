4e225c1a964a2736ee5f323e31c6239e71377c35
isalexlog <is.alex.loginov@gmail.com>
2024-06-13 07:32:01 +0200

    add tests suit

--- src/test/java/com/ninjaone/dundie_awards/DundieAwardsApplicationTests.java
+++ src/test/java/com/ninjaone/dundie_awards/DundieAwardsApplicationTests.java
@@ -1,13 +1,20 @@
 package com.ninjaone.dundie_awards;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class DundieAwardsApplicationTests {
 
+	@Autowired
+	ApplicationContext applicationContext;
+
 	@Test
 	void contextLoads() {
+		assertThat(applicationContext).isNotNull();
 	}
-
 }
